38f3114ea1f675b59e769a5c2164ec5ab45f95c8
krp312 <krp312@me.com>
2017-09-21 05:42:03 -0400

    Various degrees of progress made on the 3 sources
    * YouTube ready for the toolkit
    * SoundCloud needs the download link
    * Spotify with zero progress yet

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import soundscrape
 
 pp = pprint.PrettyPrinter(indent=4)
 
+# initial links from stereogum's /music page
 url = 'https://www.stereogum.com/music/'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -17,8 +18,20 @@ initial_links = []
 for link in links:
     initial_links.append(link.a['href'])
 
-link1 = initial_links[0]
-response1 = requests.get(link1)
-soup1 = BeautifulSoup(response1.content, 'html.parser')
-iframe = soup1.find_all('iframe', src=re.compile("soundcloud"))
-pp.pprint(iframe[0]['src'])
+final_download_links = []
+iframe_addresses = []
+
+for sublink in initial_links:
+    sublink_response = requests.get(sublink)
+    sublink_soup = BeautifulSoup(sublink_response.content, 'html.parser')
+    if sublink_soup.find('iframe', src=re.compile('soundcloud')):
+        iframe_address = sublink_soup.find(
+            'iframe', src=re.compile('soundcloud'))
+        iframe_addresses.append(iframe_address['src'])
+
+    if sublink_soup.find('iframe', src=re.compile('youtube')):
+        youtube_link = sublink_soup.find('iframe', src=re.compile('youtube'))
+        final_download_links.append(youtube_link['src'])
+
+pp.pprint(final_download_links)
+pp.pprint(iframe_addresses)
